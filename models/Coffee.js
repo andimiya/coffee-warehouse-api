312eae6f1e9923fafe9e069880eb1bce84b33d3b
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-02 21:24:10 -1000

    Added models for Coffee, Order, Customer

---
+++ models/Coffee.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+  var Coffee = sequelize.define("Coffee", {
+    name: DataTypes.STRING,
+    location: DataTypes.STRING,
+    caffeieneLevel: DataTypes.INTEGER,
+    price: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Coffee.hasMany(models.Order);
+      }
+    }
+  });
+
+  return Coffee;
+};
---
+++ models/Order.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, DataTypes) {
+  var Order = sequelize.define("Order", {
+    orderStatus: DataTypes.STRING,
+    orderTotal: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Order.belongsTo(models.Customer);
+        Order.hasMany(models.Coffee);
+      }
+    }
+  });
+
+  return Order;
+};
