cf9521510c4a129524243a4520d1249db1adcf4e
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-02 19:49:19 -1000

    Commit

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
 
-var config = {
-  appRoot: __dirname // required config
+var resolve = require('json-refs').resolveRefs;
+var YAML = require('js-yaml');
+var fs = require('fs');
+
+var root = YAML.load(fs.readFileSync('index.yaml').toString());
+var options = {
+  processContent: function (content) {
+    return YAML.load(content);
+  }
 };
+resolve(root, options).then(function (results) {
+  console.log(YAML.dump(results.resolved));
+});
+//
+// var config = {
+//   appRoot: __dirname // required config
+// };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
