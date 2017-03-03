3087f76bab6812158a52e07943bef3ca11d2d265
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-02 21:06:16 -1000

    File structure for routes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-
+app.use('/coffee', coffee);
+app.use('orders', orders);
+app.use('/customers', customers);
 
 app.listen(3000, function() {
   console.log(`Listening on ${PORT}`);
