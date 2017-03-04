f8d423d88495c4bbdad9aa329952d76dccade854
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-03 16:45:48 -1000

    Created an order migration and model

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,8 @@
+'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Order = sequelize.define("Order", {
+  var Order = sequelize.define('Order', {
     orderStatus: DataTypes.STRING,
-    orderTotal: DataTypes.STRING
+    orderTotal: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
@@ -10,6 +11,5 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   });
-
   return Order;
 };
