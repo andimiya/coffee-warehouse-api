8b753a2e8cd07a26ce13392cfbdd2f836a6aee3f
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-03 20:39:45 -1000

    Added a coffee find by id route

--- routes/coffee.js
+++ routes/coffee.js
@@ -3,8 +3,20 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   Coffee.findAll()
-  .then ( coffee => {
+  .then ( coffees => {
     res.render('pages/coffees', {
+      "coffees": coffees,
+    });
+  })
+  .catch( err => {
+    res.render('pages/error');
+  })
+});
+
+router.get('/:id', (req, res) => {
+  Coffee.findById(req.params.id)
+  .then ( coffee => {
+    res.render('pages/coffee', {
       "coffee": coffee,
     });
   })
