0e7a72e9efa02d68e08059901df1204faf126fd6
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-03 20:37:52 -1000

    Added find all coffee get route

--- routes/coffee.js
+++ routes/coffee.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('ok');
+  Coffee.findAll()
+  .then ( coffee => {
+    res.render('pages/coffees', {
+      "coffee": coffee,
+    });
+  })
+  .catch( err => {
+    res.render('pages/error');
+  })
 });
 
 module.exports = router;
