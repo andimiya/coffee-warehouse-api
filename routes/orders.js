de48efdfc38ce9baa164683322ff618ec2b80a7f
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-13 20:59:00 -1000

    Added a POST route for new Orders

---
+++ routes/orders.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const { Order } = db
+
+// router.get('/', (req, res) => {
+//   Order.findAll()
+//   .then(orders => {
+//     res.json({ orders })
+//   })
+//   .catch(() => {
+//     res.send('error')
+//   })
+// })
+
+router.post('/new', (req, res) => {
+  Order.create({
+    orderStatus: req.body.orderStatus,
+    orderTotal: req.body.orderTotal
+  })
+    .then(orders => {
+      res.send('success')
+    })
+    .catch(() => {
+      res.send('error')
+    })
+})
+
+module.exports = router
