c782ca06dd0b4e0a8d87e693d4ecb40b8d689efb
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-13 21:09:16 -1000

    Added customers route into server.js, fixed some linting errors

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const PORT = 3000;
-const handlebars = require('express-handlebars');
-const app = express();
-const bp = require('body-parser');
-const db = require('./models');
-const { Coffee, Customer, Order, CoffeeOrder } = db;
-const CONFIG = require('./config/config.json');
-const coffee = require('./routes/coffee');
-const orders = require('./routes/orders');
-// const customer = require('./routes/customers');
+const express = require('express')
+const PORT = 3000
+const handlebars = require('express-handlebars')
+const app = express()
+const bp = require('body-parser')
+const db = require('./models')
+const { Coffee, Customer, Order, CoffeeOrder } = db
+const CONFIG = require('./config/config.json')
+const coffee = require('./routes/coffee')
+const orders = require('./routes/orders')
+const customers = require('./routes/customers')
 
-//bodyparser
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
+//  bodyparser
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
-app.use('/coffee', coffee);
-app.use('/orders', orders);
-// app.use('/customers', customers);
+app.use('/coffee', coffee)
+app.use('/orders', orders)
+app.use('/customers', customers)
 
-//handlebars
+//  handlebars
 const hbs = handlebars.create({
   extname: '.hbs',
   defaultLayout: 'app'
-});
-
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-
-
-app.listen(3000, function() {
-  console.log(`Listening on ${PORT}`);
-  db.sequelize.sync();
 })
 
-module.exports = app;
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+
+app.listen(3000, function () {
+  console.log(`Listening on ${PORT}`)
+  db.sequelize.sync()
+})
+
+module.exports = app
